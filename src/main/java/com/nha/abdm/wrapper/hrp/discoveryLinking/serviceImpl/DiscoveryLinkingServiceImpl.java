6fc92509aa12763b20e66747adf48b0b1543a955
unknown <venuajitesh@gmail.com>
2024-01-10 23:54:14 +0530

    Optimized discovery logic

--- src/main/java/com/nha/abdm/wrapper/hrp/discoveryLinking/serviceImpl/DiscoveryLinkingServiceImpl.java
+++ src/main/java/com/nha/abdm/wrapper/hrp/discoveryLinking/serviceImpl/DiscoveryLinkingServiceImpl.java
@@ -2,18 +2,21 @@ package com.nha.abdm.wrapper.hrp.discoveryLinking.serviceImpl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.nha.abdm.wrapper.hrp.common.*;
+import com.nha.abdm.wrapper.hrp.common.CareContextBuilder;
+import com.nha.abdm.wrapper.hrp.common.CustomError;
+import com.nha.abdm.wrapper.hrp.common.GatewayApiPaths;
+import com.nha.abdm.wrapper.hrp.common.MakeRequest;
+import com.nha.abdm.wrapper.hrp.common.SessionManager;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.requests.OnConfirmRequest;
-import com.nha.abdm.wrapper.hrp.mongo.tables.Patients;
-import com.nha.abdm.wrapper.hrp.repository.LogsRepo;
-import com.nha.abdm.wrapper.hrp.repository.PatientRepo;
-import com.nha.abdm.wrapper.hrp.serviceImpl.LogsTableService;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.requests.OnDiscoverRequest;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.requests.OnInitRequest;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.responses.ConfirmResponse;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.responses.DiscoverResponse;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.responses.InitResponse;
 import com.nha.abdm.wrapper.hrp.discoveryLinking.services.DiscoverLinkingService;
+import com.nha.abdm.wrapper.hrp.mongo.tables.Patients;
+import com.nha.abdm.wrapper.hrp.repository.PatientRepo;
+import com.nha.abdm.wrapper.hrp.serviceImpl.LogsTableService;
 import com.nha.abdm.wrapper.hrp.serviceImpl.PatientTableService;
 import info.debatty.java.stringsimilarity.JaroWinkler;
 import org.apache.logging.log4j.LogManager;
@@ -22,9 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import java.lang.String;
+
 import java.net.URISyntaxException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -38,111 +39,47 @@ public class DiscoveryLinkingServiceImpl implements DiscoverLinkingService {
     PatientTableService patientTableService;
     @Autowired
     PatientRepo patientRepo;
-    @Autowired
-    Utils utils;
-    @Autowired
-    LogsRepo logsRepo;
 
     @Autowired
-    LogsTableService<InitResponse> logsTableService;
+    LogsTableService logsTableService;
     CustomError customError=new CustomError();
     JaroWinkler jaroWinkler = new JaroWinkler();
     ResponseEntity<ObjectNode> responseEntity;
     public void onDiscoverCall(DiscoverResponse data) throws URISyntaxException, JsonProcessingException {
-        String abhaAddress = data.getPatient().getId();
-        log.info("AbhaAddress: " + abhaAddress);
+        String receivedAbhaAddress = data.getPatient().getId();
+        String receivedPatientReference = data.getPatient().getUnverifiedIdentifiers().isEmpty()? null: data.getPatient().getUnverifiedIdentifiers().get(0).getValue();
+        String receivedYob = data.getPatient().getYearOfBirth();
+        String receivedGender = data.getPatient().getGender();
+        String receivedName = data.getPatient().getName();
+        log.info("AbhaAddress: " + receivedAbhaAddress);
         try{
-            Patients isAbhaPresent=patientRepo.findByAbhaAddress(abhaAddress);
-            Patients isMobilePresent=patientRepo.findByPatientMobile(data.getPatient().getVerifiedIdentifiers().get(0).getValue());
-            Patients isPatientIdentifier=null;
-            if(!data.getPatient().getUnverifiedIdentifiers().isEmpty()) isPatientIdentifier=patientRepo.findByPatientReference(data.getPatient().getUnverifiedIdentifiers().get(0).getValue());
-            if(Objects.nonNull(isAbhaPresent)){
+            Patients isAbhaPresent=patientRepo.findByAbhaAddress(receivedAbhaAddress);
+            List<Patients> isMobilePresent=patientRepo.findByPatientMobile(data.getPatient().getVerifiedIdentifiers().get(0).getValue());
+            Patients isPatientIdentifier=receivedPatientReference!=null?patientRepo.findByPatientReference(receivedPatientReference):null;
+            if(Objects.nonNull(isAbhaPresent)) {
                 log.info("Patient matched with AbhaAddress");
                 List<CareContextBuilder> careContexts = isAbhaPresent.getCareContexts().stream()
                         .filter(context -> !context.isLinked())
                         .collect(Collectors.toList());
-                makeBody(data,isAbhaPresent,careContexts);
-            }else if(Objects.nonNull(isMobilePresent)){
-                log.info("Patient matched with Mobile");
-                String existingGender=isMobilePresent.getGender();
-                int existingDate=Integer.parseInt(isMobilePresent.getDateOfBirth().substring(0,4));
-                log.info(existingDate +" given yob : "+data.getPatient().getYearOfBirth()+ " difference : "+ Math.abs(existingDate-Integer.parseInt(data.getPatient().getYearOfBirth()))+" name :"+ jaroWinkler.similarity(isMobilePresent.getName(),data.getPatient().getName()));
-                if(existingGender.equals(data.getPatient().getGender()) && Math.abs(existingDate-Integer.parseInt(data.getPatient().getYearOfBirth()))<=5 && jaroWinkler.similarity(isMobilePresent.getName(),data.getPatient().getName())>=0.5){//&& soundex.encode(isMobilePresent.getName()).equals(data.getPatient().getName())
-                    List<CareContextBuilder> careContexts = isMobilePresent.getCareContexts().stream()
-                            .filter(context -> !context.isLinked())
-                            .collect(Collectors.toList());
-                    makeBody(data,isMobilePresent,careContexts);
-
-                }else{
+                makeBody(data, isAbhaPresent, careContexts);
+            }else if(!isMobilePresent.isEmpty()){
+                for(Patients item: isMobilePresent){
+                    log.info(item.toString());
+                }
+                Optional<Patients> matchingPatient = findMatchingPatient(isMobilePresent, receivedPatientReference,
+                        receivedGender, receivedYob, receivedName);
+                if (matchingPatient.isPresent()) {
+                    makeBody(data,matchingPatient.get(),matchingPatient.get().getCareContexts());
+                } else {
                     customError.setCode(1000);
                     customError.setMessage("HIP -> Details mismatch with mobile");
                     noPatient(data,customError);
                 }
-            }else if(Objects.nonNull(isPatientIdentifier)){
-                log.info("Patient matched with Patient Identifier");
-                String existingGender=isPatientIdentifier.getGender();
-                int existingDate=Integer.parseInt(isPatientIdentifier.getDateOfBirth().substring(0,4));
-                log.info(existingDate +" given yob : "+data.getPatient().getYearOfBirth()+ " difference : "+ Math.abs(existingDate-Integer.parseInt(data.getPatient().getYearOfBirth()))+" name :"+ jaroWinkler.similarity(isPatientIdentifier.getName(),data.getPatient().getName()));
-                if(existingGender.equals(data.getPatient().getGender()) && Math.abs(existingDate-Integer.parseInt(data.getPatient().getYearOfBirth()))<=5&& jaroWinkler.similarity(isPatientIdentifier.getName(),data.getPatient().getName())>=0.5) {// && soundex.encode(isMobilePresent.getName()).equals(data.getPatient().getName()
-                    List<CareContextBuilder> careContexts = isPatientIdentifier.getCareContexts().stream()
-                            .filter(context -> !context.isLinked())
-                            .collect(Collectors.toList());
-                    makeBody(data, isPatientIdentifier, careContexts);
-                }else{
-                    customError.setCode(1000);
-                    customError.setMessage("HIP : Details mismatch with patientIdentifier");
-                    noPatient(data,customError);
-                }
-            }else {
-                customError.setCode(1000);
-                customError.setMessage("HIP -> Patient Not found");
-                noPatient(data,customError);
             }
         }catch(Exception e){
             log.error("OnDiscover : "+ Arrays.toString(e.getStackTrace()));
         }
 
-    }
-    public void makeBody(DiscoverResponse data,Patients patients,List<CareContextBuilder> careContexts) throws URISyntaxException, JsonProcessingException {
-        String abhaAddress=data.getPatient().getId();
-        String patientMobile= patients.getPatientMobile();
-        String referenceNumber= patients.getPatientReference();
-        String display=patients.getDisplay();
-        HttpEntity<ObjectNode> requestEntity = OnDiscoverRequest.builder()
-                .data(data)
-                .abhaAddress(abhaAddress)
-                .referenceNumber(referenceNumber)
-                .display(display)
-                .careContexts(careContexts)
-                .sessionManager(sessionManager)
-                .build()
-                .makeRequest();
-        try {
-            ResponseEntity<ObjectNode> responseEntity=MakeRequest.post(GatewayApiPaths.ON_DISCOVER,requestEntity);
-            log.info(GatewayApiPaths.ON_DISCOVER+" : onDiscoverCall: " + responseEntity.getStatusCode());
-            logsTableService.setContent(data,requestEntity, DiscoverResponse.class);
-        } catch (Exception e) {
-            log.info("Error: " + e);
-        }
-    }
-    public void noPatient(DiscoverResponse data,CustomError customError){
-        try {
-            HttpEntity<ObjectNode> requestEntity = OnDiscoverRequest.builder()
-                    .data(data)
-                    .abhaAddress(data.getPatient().getId())
-                    .referenceNumber(null)
-                    .display(null)
-                    .careContexts(Collections.emptyList())
-                    .sessionManager(sessionManager)
-                    .customError(customError)
-                    .build()
-                    .makeRequest();
-            ResponseEntity<ObjectNode> responseEntity=MakeRequest.post(GatewayApiPaths.ON_DISCOVER,requestEntity);
-
-            log.info("Discover: requestId : " + data.getRequestId() + ": Patient not found");
-        } catch (Exception e) {
-            log.info(e);
-        }
     }
     public void onInitCall(InitResponse data) throws URISyntaxException, JsonProcessingException {
         boolean isCareContextPresent=patientTableService.checkCareContexts(data);
@@ -178,8 +115,6 @@ public class DiscoveryLinkingServiceImpl implements DiscoverLinkingService {
         }
     }
 
-
-
     public void onConfirmCall(ConfirmResponse data) throws URISyntaxException, JsonProcessingException {
         List<CareContextBuilder> careContexts=null;
         String referenceNumber=null;
@@ -203,23 +138,88 @@ public class DiscoveryLinkingServiceImpl implements DiscoverLinkingService {
         }
         List<CareContextBuilder> selectedCareContexts = logsTableService.getSelectedCareContexts(linkRefNumber,careContexts);
         try {
-        HttpEntity<ObjectNode> requestEntity = OnConfirmRequest.builder()
-                .data(data)
-                .abhaAddress(abhaAddress)
-                .referenceNumber(patientReference)
-                .display(display)
-                .careContexts(selectedCareContexts)
-                .linkRefNumber(linkRefNumber)
-                .sessionManager(sessionManager)
-                .build()
-                .makeRequest();
+            HttpEntity<ObjectNode> requestEntity = OnConfirmRequest.builder()
+                    .data(data)
+                    .abhaAddress(abhaAddress)
+                    .referenceNumber(patientReference)
+                    .display(display)
+                    .careContexts(selectedCareContexts)
+                    .linkRefNumber(linkRefNumber)
+                    .sessionManager(sessionManager)
+                    .build()
+                    .makeRequest();
 
-            ResponseEntity<ObjectNode> responseEntity=MakeRequest.post(GatewayApiPaths.ON_CONFIRM,requestEntity);
-        log.info(GatewayApiPaths.ON_CONFIRM+" : onConfirmCall: " + responseEntity.getStatusCode());
-        patientTableService.updateCareContextStatus(patientReference,selectedCareContexts);
+            responseEntity=MakeRequest.post(GatewayApiPaths.ON_CONFIRM,requestEntity);
+            log.info(GatewayApiPaths.ON_CONFIRM+" : onConfirmCall: " + responseEntity.getStatusCode());
+            patientTableService.updateCareContextStatus(patientReference,selectedCareContexts);
         }
         catch (Exception e){
             log.error(GatewayApiPaths.ON_CONFIRM+" : OnConfirmCall -> Error :"+ Arrays.toString(e.getStackTrace()));
         }
+    }
+    //Helpers
+    public void makeBody(DiscoverResponse data,Patients patients,List<CareContextBuilder> careContexts) throws URISyntaxException, JsonProcessingException {
+        log.info("MakingBody :" +patients.toString());
+        String abhaAddress=data.getPatient().getId();
+        String patientMobile= patients.getPatientMobile();
+        String referenceNumber= patients.getPatientReference();
+        String display=patients.getDisplay();
+        HttpEntity<ObjectNode> requestEntity = OnDiscoverRequest.builder()
+                .data(data)
+                .abhaAddress(abhaAddress)
+                .referenceNumber(referenceNumber)
+                .display(display)
+                .careContexts(careContexts)
+                .sessionManager(sessionManager)
+                .build()
+                .makeRequest();
+        try {
+            responseEntity=MakeRequest.post(GatewayApiPaths.ON_DISCOVER,requestEntity);
+            log.info(GatewayApiPaths.ON_DISCOVER+" : onDiscoverCall: " + responseEntity.getStatusCode());
+            logsTableService.setContent(data,requestEntity, DiscoverResponse.class);
+        } catch (Exception e) {
+            log.info("Error: " + e);
+        }
+    }
+    public void noPatient(DiscoverResponse data,CustomError customError){
+        try {
+            HttpEntity<ObjectNode> requestEntity = OnDiscoverRequest.builder()
+                    .data(data)
+                    .abhaAddress(data.getPatient().getId())
+                    .referenceNumber(null)
+                    .display(null)
+                    .careContexts(Collections.emptyList())
+                    .sessionManager(sessionManager)
+                    .customError(customError)
+                    .build()
+                    .makeRequest();
+            responseEntity=MakeRequest.post(GatewayApiPaths.ON_DISCOVER,requestEntity);
+            log.info("Discover: requestId : " + data.getRequestId() + ": Patient not found");
+        } catch (Exception e) {
+            log.info(e);
+        }
+    }
+    private Optional<Patients> findMatchingPatient(List<Patients> isMobilePresent, String receivedPatientReference, String receivedGender, String receivedYob, String receivedName) {
+        if (receivedPatientReference != null) {
+            return isMobilePresent.stream()
+                    .filter(patient -> receivedPatientReference.equals(patient.getPatientReference()))
+                    .findFirst();
+        }else{
+            return isMobilePresent.stream()
+                    .filter(patient -> isGenderMatch(patient,receivedGender))
+                    .filter(patient -> isYOBInRange(patient, receivedYob))
+                    .filter(patient -> isFuzzyNameMatch(patient, receivedName))
+                    .findFirst();
+        }
+    }
+    private boolean isYOBInRange(Patients patient, String receivedYob) {
+        int existingDate=Integer.parseInt(patient.getDateOfBirth().substring(0,4));
+        return Math.abs(existingDate-Integer.parseInt(receivedYob))<=5;
+    }
+    private boolean isGenderMatch(Patients patient, String receivedGender) {
+        return receivedGender.equals(patient.getGender());
+    }
+    private boolean isFuzzyNameMatch(Patients patient, String receivedName) {
+        return jaroWinkler.similarity(patient.getName(),receivedName)>=0.5;
     }
 }
