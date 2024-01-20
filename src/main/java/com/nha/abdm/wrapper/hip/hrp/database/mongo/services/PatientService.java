c39319f0a56d6396d824425b47c307897f0fa80b
unknown <venuajitesh@gmail.com>
2024-01-21 00:55:46 +0530

    Added javaDoc

--- src/main/java/com/nha/abdm/wrapper/hip/hrp/database/mongo/services/PatientService.java
+++ src/main/java/com/nha/abdm/wrapper/hip/hrp/database/mongo/services/PatientService.java
@@ -1,14 +1,14 @@
 /* (C) 2024 */
 package com.nha.abdm.wrapper.hip.hrp.database.mongo.services;
 
-import com.nha.abdm.wrapper.common.models.AckRequest;
 import com.nha.abdm.wrapper.common.models.CareContext;
+import com.nha.abdm.wrapper.common.models.FacadeResponse;
 import com.nha.abdm.wrapper.hip.hrp.common.requests.CareContextRequest;
 import com.nha.abdm.wrapper.hip.hrp.database.mongo.repositories.LogsRepo;
 import com.nha.abdm.wrapper.hip.hrp.database.mongo.repositories.PatientRepo;
 import com.nha.abdm.wrapper.hip.hrp.database.mongo.tables.Patient;
-import com.nha.abdm.wrapper.hip.hrp.hipLink.responses.LinkRecordsResponse;
-import com.nha.abdm.wrapper.hip.hrp.link.responses.InitResponse;
+import com.nha.abdm.wrapper.hip.hrp.link.hipInitiated.responses.LinkRecordsResponse;
+import com.nha.abdm.wrapper.hip.hrp.link.userInitiated.responses.InitResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,21 +39,43 @@ public class PatientService {
     this.patientRepo = patientRepo;
   }
 
+  /**
+   * Fetch of patientReference using abhaAddress
+   *
+   * @param abhaAddress abhaAddress of patient.
+   * @return patientReference.
+   */
   public String getPatientReference(String abhaAddress) {
     Patient existingRecord = this.patientRepo.findByAbhaAddress(abhaAddress);
     return existingRecord != null ? existingRecord.getPatientReference() : "";
   }
-
+  /**
+   * Fetch of patientDisplay using abhaAddress
+   *
+   * @param abhaAddress abhaAddress of patient.
+   * @return patientDisplay.
+   */
   public String getPatientDisplay(String abhaAddress) {
     Patient existingRecord = this.patientRepo.findByAbhaAddress(abhaAddress);
     return existingRecord != null ? existingRecord.getDisplay() : "";
   }
-
+  /**
+   * Fetch of abhaAddress using abhaAddress
+   *
+   * @param patientReference patientReference of patient.
+   * @return abhaAddress.
+   */
   public String getAbhaAddress(String patientReference) {
     Patient existingRecord = this.patientRepo.findByPatientReference(patientReference);
     return existingRecord != null ? existingRecord.getAbhaAddress() : "";
   }
 
+  /**
+   * After successful linking of careContext updating the status i.e. isLinked to true or false.
+   *
+   * @param abhaAddress abhaAddress of patient.
+   * @param careContexts List of careContext to update the status.
+   */
   public void updateCareContextStatus(String abhaAddress, List<CareContextRequest> careContexts) {
     BulkOperations bulkOperations =
         mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Patient.class);
@@ -73,17 +95,25 @@ public class PatientService {
     bulkOperations.execute();
   }
 
-  public boolean checkCareContexts(InitResponse data) {
+  /**
+   * <B>discovery</B>
+   *
+   * <p>Match the response careContexts with patient careContexts.
+   *
+   * @param initResponse Response from ABDM gateway to link particular careContexts.
+   * @return if careContexts matches returns returns true else false.
+   */
+  public boolean checkCareContexts(InitResponse initResponse) {
     try {
       List<CareContext> patientCareContexts =
           patientRepo
-              .findByPatientReference(data.getPatient().getReferenceNumber())
+              .findByPatientReference(initResponse.getPatient().getReferenceNumber())
               .getCareContexts();
       Set<String> patientReferenceNumbers =
           patientCareContexts.stream()
               .map(CareContext::getReferenceNumber)
               .collect(Collectors.toSet());
-      return data.getPatient().getCareContexts().stream()
+      return initResponse.getPatient().getCareContexts().stream()
           .allMatch(
               responseContext ->
                   patientReferenceNumbers.contains(responseContext.getReferenceNumber()));
@@ -94,15 +124,15 @@ public class PatientService {
     return true;
   }
 
-  public AckRequest addPatient(LinkRecordsResponse data) {
-    String abhaAddress = data.getAbhaAddress();
+  public FacadeResponse addPatient(LinkRecordsResponse linkRecordsResponse) {
+    String abhaAddress = linkRecordsResponse.getAbhaAddress();
     try {
       Patient existingRecord = this.patientRepo.findByAbhaAddress(abhaAddress);
       if (existingRecord == null) {
-        throw new RuntimeException("Patient not found");
+        log.error("Adding patient failed -> Patient not found");
       } else {
         List<Map<String, Object>> modifiedCareContexts =
-            data.getPatient().getCareContexts().stream()
+            linkRecordsResponse.getPatient().getCareContexts().stream()
                 .map(
                     careContext -> {
                       Map<String, Object> modifiedContext = new HashMap<>();
@@ -112,44 +142,51 @@ public class PatientService {
                       return modifiedContext;
                     })
                 .collect(Collectors.toList());
-        Query query = new Query(Criteria.where("abhaAddress").is(data.getAbhaAddress()));
-        Update update = new Update().addToSet("careContext").each(modifiedCareContexts); // TODO
+        Query query =
+            new Query(Criteria.where("abhaAddress").is(linkRecordsResponse.getAbhaAddress()));
+        Update update = new Update().addToSet("careContext").each(modifiedCareContexts);
         this.mongoTemplate.updateFirst(query, update, Patient.class);
       }
     } catch (Exception e) {
       log.info("addPatient :" + e);
     }
-    return AckRequest.builder().message("Successfully Added Patient").build();
+    return FacadeResponse.builder().message("Successfully Added Patient").build();
   }
 
-  public AckRequest addPatientInWrapper(Patient data) {
-    Patient existingRecord = patientRepo.findByAbhaAddress(data.getAbhaAddress());
+  /**
+   * Store/update patient demographic data.
+   *
+   * @param patient patient demographic details.
+   * @return acknowledgement of storing of patient.
+   */
+  public FacadeResponse addPatientInWrapper(Patient patient) {
+    Patient existingRecord = patientRepo.findByAbhaAddress(patient.getAbhaAddress());
     if (existingRecord == null) {
       Patient newRecord = new Patient();
-      newRecord.setName(data.getName());
-      newRecord.setAbhaAddress(data.getAbhaAddress());
-      newRecord.setPatientReference(data.getPatientReference());
-      newRecord.setGender(data.getGender());
-      newRecord.setDateOfBirth(data.getDateOfBirth());
-      newRecord.setDisplay(data.getDisplay());
-      newRecord.setPatientMobile(data.getPatientMobile());
+      newRecord.setName(patient.getName());
+      newRecord.setAbhaAddress(patient.getAbhaAddress());
+      newRecord.setPatientReference(patient.getPatientReference());
+      newRecord.setGender(patient.getGender());
+      newRecord.setDateOfBirth(patient.getDateOfBirth());
+      newRecord.setDisplay(patient.getDisplay());
+      newRecord.setPatientMobile(patient.getPatientMobile());
       mongoTemplate.save(newRecord);
-      log.info("Successfully Added Patient : " + data.toString());
+      log.info("Successfully Added Patient : " + patient.toString());
 
     } else {
       Update update =
           new Update()
-              .set("abhaAddress", data.getAbhaAddress())
-              .set("name", data.getName())
-              .set("gender", data.getGender())
-              .set("dateOfBirth", data.getDateOfBirth())
-              .set("display", data.getDisplay())
-              .set("patientReference", data.getPatientReference())
-              .set("patientMobile", data.getPatientMobile());
-      Query query = new Query(Criteria.where("abhaAddress").is(data.getAbhaAddress()));
+              .set("abhaAddress", patient.getAbhaAddress())
+              .set("name", patient.getName())
+              .set("gender", patient.getGender())
+              .set("dateOfBirth", patient.getDateOfBirth())
+              .set("display", patient.getDisplay())
+              .set("patientReference", patient.getPatientReference())
+              .set("patientMobile", patient.getPatientMobile());
+      Query query = new Query(Criteria.where("abhaAddress").is(patient.getAbhaAddress()));
       mongoTemplate.updateFirst(query, update, Patient.class);
-      log.info("Successfully Updated Patient : " + data.toString());
-      return AckRequest.builder().message("Successfully Updated Patient").build();
+      log.info("Successfully Updated Patient : " + patient.toString());
+      return FacadeResponse.builder().message("Successfully Updated Patient").build();
     }
     return null;
   }
---
+++ src/main/java/com/nha/abdm/wrapper/hip/hrp/link/hipInitiated/responses/LinkOnAddCareContextsResponse.java
@@ -0,0 +1,24 @@
+/* (C) 2024 */
+package com.nha.abdm.wrapper.hip.hrp.link.hipInitiated.responses;
+
+import com.nha.abdm.wrapper.common.ErrorResponse;
+import com.nha.abdm.wrapper.hip.hrp.discover.requests.Response;
+import com.nha.abdm.wrapper.hip.hrp.link.hipInitiated.responses.helpers.LinkAcknowledgement;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class LinkOnAddCareContextsResponse implements Serializable {
+  private static final long serialVersionUID = 165269402517398406L;
+  //    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+  private String timestamp;
+  private LinkAcknowledgement acknowledgement;
+  private ErrorResponse error;
+  private Response resp;
+}
