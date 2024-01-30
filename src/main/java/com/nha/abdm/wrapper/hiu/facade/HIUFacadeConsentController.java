567408932e9a97bfdb462cd3622b6e94c577ef2a
Atul Kumar <atulai@google.com>
2024-01-30 14:56:10 +0000

    Added methods to Consent Facade Controller

--- src/main/java/com/nha/abdm/wrapper/hiu/facade/HIUFacadeConsentController.java
+++ src/main/java/com/nha/abdm/wrapper/hiu/facade/HIUFacadeConsentController.java
@@ -3,7 +3,10 @@ package com.nha.abdm.wrapper.hiu.facade;
 
 import com.nha.abdm.wrapper.common.responses.FacadeResponse;
 import com.nha.abdm.wrapper.hiu.hrp.consent.HIUConsentInterface;
+import com.nha.abdm.wrapper.hiu.hrp.consent.requests.ConsentStatusRequest;
+import com.nha.abdm.wrapper.hiu.hrp.consent.requests.FetchConsentRequest;
 import com.nha.abdm.wrapper.hiu.hrp.consent.requests.InitConsentRequest;
+import com.nha.abdm.wrapper.hiu.hrp.consent.requests.OnNotifyRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +17,24 @@ public class HIUFacadeConsentController {
 
   @Autowired private HIUConsentInterface hiuConsentInterface;
 
-  @PostMapping({"/consents-init"})
+  @PostMapping({"/consent-init"})
   public FacadeResponse initiateConsentRequest(@RequestBody InitConsentRequest initConsentRequest) {
     return hiuConsentInterface.initiateConsentRequest(initConsentRequest);
+  }
+
+  @PostMapping({"/consent-status"})
+  public FacadeResponse consentRequestStatus(
+      @RequestBody ConsentStatusRequest consentStatusRequest) {
+    return hiuConsentInterface.consentRequestStatus(consentStatusRequest);
+  }
+
+  @PostMapping({"/hiu-on-notify"})
+  public FacadeResponse hiuOnNotify(@RequestBody OnNotifyRequest onNotifyRequest) {
+    return hiuConsentInterface.hiuOnNotify(onNotifyRequest);
+  }
+
+  @PostMapping({"/fetch-consent"})
+  public FacadeResponse fetchConsent(@RequestBody FetchConsentRequest fetchConsentRequest) {
+    return hiuConsentInterface.fetchConsent(fetchConsentRequest);
   }
 }
